8302aad70bf22650e7bea759e7a2cd9cbd088334
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-01 19:18:19 +0100

    Update NHTP.m

--- CSpack/solvers/funcs/NHTP.m
+++ CSpack/solvers/funcs/NHTP.m
@@ -1,16 +1,18 @@
 function Out = NHTP(data,n,s,pars)
 
-% This code aims at solving the sparsity constrained optimization with form
+% This code aims at solving the sparsity constrained CS:
 %
-%         min_{x\in R^n} f(x)  s.t.  \|x\|_0<=s
+%         min_{x\in R^n} 0.5||Ax-b||^2,  s.t.  \|x\|_0<=s
 %
 % where s is the given sparsity, which is << n.  
-%
+%       A\in\R{m by n} the measurement matrix
+%       b\in\R{m by 1} the observation vector 
+%=========================================================================
 % Inputs:
-%     data    : A triple structure (data.A, data.At, data.b) (required)
-%               data.A, the measurement matrix, or a function handle @(x)A(x);
-%               data.At = data.A',or a function handle @(x)At(x);
-%               data.b, the observation vector 
+%     data    : A structure (required)
+%               (data.A, data.b) if A is a matrix 
+%               (data.A, data.b, data.At) if A is a function handle
+%               i.e., Ax = data.A(x); A'y = data.At(y); 
 %     n       : Dimension of the solution x, (required)
 %     s       : Sparsity level of x, an integer between 1 and n-1, (required)           
 %     pars:     Parameters are all OPTIONAL
@@ -27,33 +29,47 @@ function Out = NHTP(data,n,s,pars)
 %     Out.time           CPU time
 %     Out.iter:          Number of iterations
 %     Out.obj:           Objective function value at Out.sol 
-%
+%==========================================================================
 % This code is programmed based on the algorithm proposed in 
 % "S. Zhou, N. Xiu and H. Qi, Global and Quadratic Convergence of Newton 
-% Hard-Thresholding Pursuit, Journal of Machine Learning Research, 2019."
+% Hard-Thresholding Pursuit, Journal of Machine Learning Research, 2021."
 % Send your comments and suggestions to <slzhou2021@163.com> 
 % Warning: Accuracy may not be guaranteed !!!!! ! 
+% ========================================================================= 
 
 warning off;
 t0  = tic;
 if  nargin<3
     fprintf(' No enough inputs. No problems will be solverd!'); return;
 end
+if ~isfield(data,'A')
+    fprintf('<data.A> is missing, unable to run the solver ...');
+    return
+else
+    if  isa(data.A,'function_handle') && ~isfield(data,'At') 
+        fprintf('<data.At> is missing, unable to run the solver ...');
+        return
+    end
+end
+
+if  ~isfield(data,'b')
+    fprintf('<data.b> is missing, unable to run the solver ...');
+    return
+end
+
 if nargin < 4; pars = struct([]);  end 
 if isfield(pars,'disp');   disp    = pars.disp;   else; disp    = 1;    end
-if isfield(pars,'draw');   draw    = pars.draw;   else; draw    = 0;    end
 if isfield(pars,'maxit');  itmax   = pars.maxit;  else; itmax   = 2000; end
 if isfield(pars,'tol');    tol     = pars.tol;    else; tol     = 1e-6; end  
 if isfield(pars,'x0');     x0      = pars.x0;     else; x0 = zeros(n,1);end 
 
 if isstruct(data);  data.n = n; end
-func    = @(x,key,T1,T2)compressed_sensing(x,key,T1,T2,data); 
+func           = @(x,T1,T2)CS(x,T1,T2,data); 
 [x0,obj,g,eta] = getparameters(n,s,x0,func,pars);  
 
 x       = x0;
 beta    = 0.5;
 sigma   = 5e-5;
-I       = 1:n;
 delta   = 1e-10;
 pcgtol  = 0.1*tol*s;
 T0      = [];
@@ -82,7 +98,7 @@ if  max(isnan(g))
     x0      = zeros(n,1);
     rind    = randi(n);
     x0(rind)= rand;
-    [obj,g] = func(x0,'ObjGrad',[],[]);
+    [obj,g] = func(x0,[],[]);
 end
  
 % The main body  
@@ -114,11 +130,11 @@ for iter = 1:itmax
     
     % update next iterate
     if  iter   == 1 || flag           % update next iterate if T==supp(x^k)     
-        H       =  func(x0,'Hess',T,[]); 
+        H       =  func(x0,T,[]); 
         if ~isa(H,'function_handle')
             d   = -H\gT;
         else
-           [d,~]= pcg(H,-gT,pcgtol,50); 
+            d   = my_cg(H,-gT,pcgtol,50,zeros(s,1)); 
         end
         dg      = sum(d.*gT);
         ngT     = FNorm(gT);
@@ -127,7 +143,7 @@ for iter = 1:itmax
         dg      = ngT; 
         end
     else                              % update next iterate if T~=supp(x^k) 
-        [H,D]   = func(x0,'Hess',T,TTc);
+        [H,D]   = func(x0,T,TTc);
         
         if isa(D,'function_handle')
            Dx   = D(x0(TTc));
@@ -138,7 +154,7 @@ for iter = 1:itmax
         if ~isa(H,'function_handle')
             d   = H\( Dx-gT);
         else
-           [d,~]= pcg(H,Dx-gT, pcgtol,50); 
+            d   = my_cg(H,Dx-gT,pcgtol,50,zeros(s,1)); 
         end
         
         Fnz     = FNorm(x(TTc))/4/eta;
@@ -163,7 +179,7 @@ for iter = 1:itmax
     % Amijio line search
     for i      = 1:6
         x(T)   = x0(T) + alpha*d;
-        obj    = func(x,'ObjGrad',[],[]);
+        obj    = func(x,[],[]);
         if obj < obj0  + alpha*sigma*dg; break; end        
         alpha  = beta*alpha;
     end
@@ -172,7 +188,7 @@ for iter = 1:itmax
     fhtp    = 0;
     if obj  > obj0 
        x(T) = xtg(T); 
-       obj  = func(x,'ObjGrad',[],[]); 
+       obj  = func(x,[],[]); 
        fhtp = 1;
     end
     
@@ -190,7 +206,7 @@ for iter = 1:itmax
  
     T0      = T; 
     x0      = x; 
-    [obj,g] = func(x,'ObjGrad',[],[]);
+    [obj,g] = func(x,[],[]);
     
     % Update eta
     if  mod(iter,50)==0  
@@ -203,26 +219,13 @@ for iter = 1:itmax
     end     
 end
 
-
-x(abs(x)<1e-4)=0;
-[obj ,g]    = func(x,'ObjGrad',[],[]);
 % results output
 time        = toc(t0);
 Out.sp      = nnz(x);
 Out.time    = time;
 Out.iter    = iter;
 Out.sol     = x;
-Out.obj     = obj; 
-
-if  draw
-    figure
-    subplot(121) 
-    Obj(iter)= obj;  
-    PlotFun(Obj,iter,'r.-','f(x^k)'); 
-    subplot(122) 
-    PlotFun(Error,iter,'r.-','error') 
-end
-
+Out.obj     = obj;  
 
 if disp 
    fprintf(' ------------------------------------------------\n');
@@ -245,8 +248,8 @@ end
 function [x0,obj,g,eta]=getparameters(n,s,x0,func,pars)
 
     if isfield(pars,'x0') && norm(x0)>0
-       [obj0,g0] = func(zeros(n,1),'ObjGrad',[],[]);  
-       [obj,g]   = func(pars.x0,'ObjGrad',[],[]); 
+       [obj0,g0] = func(zeros(n,1),[],[]);  
+       [obj,g]   = func(pars.x0,[],[]); 
        if obj0   < obj/10
           x0     =  zeros(n,1); 
           obj    = obj0;  
@@ -273,14 +276,14 @@ function [x0,obj,g,eta]=getparameters(n,s,x0,func,pars)
           
        end
     else
-        [obj,g]  = func(x0,'ObjGrad',[],[]); 
+        [obj,g]  = func(x0,[],[]); 
     end
  
     
     if isfield(pars,'eta')      
         eta  = pars.eta;       
     else % set a proper parameter eta
-        [~,g1] = func(ones(n,1),'ObjGrad',[],[]) ;
+        [~,g1] = func(ones(n,1),[],[]) ;
         abg1   = abs(g1);
         T      = find(abg1>1e-8);
         maxe   = sum(1./(abg1(T)+eps))/nnz(T);
@@ -299,77 +302,69 @@ function [x0,obj,g,eta]=getparameters(n,s,x0,func,pars)
     
 end
 
-% plot the graph: iter v.s. obj (or error) --------------------------------
-function  PlotFun(input,iter,c, key) 
-    if  input(iter)>1e-40 && input(iter)<1e-5
-        semilogy(1:iter,input(1:iter),c,'MarkerSize',7,'LineWidth',1);
-    else
-        plot(1:iter,input(1:iter),c,'MarkerSize',7,'LineWidth',1);
-    end
-    xlabel('Iter'); ylabel(key); grid on    
-    
-end
 
 
 % define functions --------------------------------------------------------
-function [out1,out2] = compressed_sensing(x,fgh,T1,T2,data)
+function [out1,out2] = CS(x,T1,T2,data)
 
-if ~isa(data.A, 'function_handle')             % A is a matrix 
-    Tx = find(x); 
-    Ax = data.A(:,Tx)*x(Tx);
-    switch fgh        
-    case 'ObjGrad'
-        Axb  = Ax-data.b;
-        out1 = sum(Axb.*Axb)/2;                % objective function value of f
-        if  nargout>1 
-        out2 = data.At*Axb;                    % gradien of f
-        end
-    case 'Hess'       
-        if  length(T1)<2000 
-            out1 = data.At(T1,:)*data.A(:,T1);     %subHessian containing T1 rows and T1 columns
+if ~isa(data.A, 'function_handle') % A is a matrix 
+    if  isempty(T1) && isempty(T2) 
+        if  nnz(x) >= 0.8*length(x)
+            Axb     = data.A*x-data.b;
         else
-            AT1  = data.A(:,T1);
-            out1 = @(v)((AT1*v)'*AT1)';      
+            Tx      = find(x); 
+            Axb     = data.A(:,Tx)*x(Tx)-data.b;
         end
-        
-        if nargout>1
-        out2 = @(v)(data.At(T1,:)*(data.A(:,T2)*v)); %subHessian containing T1 rows and T2 columns
+            out1    = (Axb'*Axb)/2;               % objective function value of f
+        if  nargout == 2
+            out2    = (Axb'*data.A)';                % gradien of f
+        end
+    else        
+            AT = data.A(:,T1); 
+        if  length(T1)<3000
+            out1 = AT'*AT;                        %subHessian containing T1 rows and T1 columns
+        else
+            out1 = @(v)( (AT*v)'*AT )';      
+        end       
+        if  nargout == 2
+            out2 = @(v)( (data.A(:,T2)*v)'*AT )'; %subHessian containing T1 rows and T2 columns
+        end       
+    end
+else  % A is a function handle A*x=A(x)  
+    if ~isfield(data,'At') 
+        disp('The transpose-data.At-is missing'); return; 
+    end
+    if ~isfield(data,'n')  
+        disp('The dimension-data.n-is missing');  return;  
+    end   
+    if  isempty(T1) && isempty(T2)  
+        Axb  = data.A(x)-data.b;
+        out1 = (Axb'*Axb)/2;              % objective function value of f
+        if  nargout>1 
+            out2 = data.At(Axb);          % gradien of f
+        end
+    else
+        func = fgH(data);    
+        out1 = @(v)func(v,T1,T1);         % subHessian containing T1 rows and T1 columns
+        if  nargout>1
+            out2 = @(v)func(v,T1,T2);     % subHessian containing T1 rows and T1 columns
         end  
         
     end
-else                                       % A is a function handle A*x=A(x)
-    func = fgH(data);
-    switch  fgh        
-      case 'ObjGrad'
-            out1 = func.obj(x);                % objective function value of f
-            if  nargout>1 
-            out2 = func.grad(x);               % gradien of f
-            end
-      case 'Hess'
-            out1 = @(v)func.Hess(v,T1,T1);     % subHessian containing T1 rows and T1 columns
-            if nargout>1
-            out2 = @(v)func.Hess(v,T1,T2);     % subHessian containing T1 rows and T1 columns
-            end  
-        
-    end
 end
 
 end
 
-function func = fgH(data)
-    Axb       = @(z)data.A(z)-data.b;
-    func.obj  = @(z)norm(Axb(z))^2/2;              
-    func.grad = @(z)data.At(Axb(z));  
+function Hess = fgH(data)
     suppz     = @(z,t)supp(data.n,z,t);
     sub       = @(z,t)z(t,:);
-    func.Hess = @(z,t1,t2)(sub( data.At( data.A(suppz(z,t2))),t1)); 
+    Hess      = @(z,t1,t2)(sub( data.At( data.A(suppz(z,t2))),t1)); 
 end
 
-function z=supp(n,x,T)
-    z    = zeros(n,1);
-    z(T) = x;
+function z = supp(n,x,T)
+    z      = zeros(n,1);
+    z(T)   = x;
 end
-
 
 % conjugate gradient-------------------------------------------------------
 function x = my_cg(fx,b,cgtol,cgit,x)
