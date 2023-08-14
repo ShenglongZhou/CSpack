e5dba369e3209e0cc8195be8d452b3a9d6862951
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2023-08-14 20:15:59 +0800

    Update demonRAND.m

--- CSpack/demonRAND.m
+++ CSpack/demonRAND.m
@@ -1,8 +1,8 @@
 % demon compressed sensing problems with random data 
 clc; clear; close all;
 
-n       = 10000;  
-m       = ceil(n/4); 
+n       = 20000;  
+m       = ceil(0.25*n); 
 s       = ceil(0.05*n); 
 
 % generate the testing data (data.A, data.At, data.b)
@@ -15,7 +15,7 @@ data.b  = data.A(:,I)*xopt(I);
 % choose one of the following four solvers  
 t       = 1; 
 solver  = {'NHTP', 'GPNP', 'IIHT', 'NL0R', 'MIRL1'};
-pars.s  = s; % required for solvers 'NHTP' and 'IIHT' 
+pars.s  = s; % required for solvers 'NHTP'，'GPNP', and 'IIHT' 
 out     = CSsolver(data,n,solver{t},pars); 
 
 % results output and recovery display 
@@ -23,5 +23,3 @@ fprintf(' CPU time:     %.3fsec\n',out.time);
 fprintf(' Objective:    %.2e\n', out.obj);
 fprintf(' Sample size:  %dx%d\n', m,n);
 RecoverShow(xopt,out.sol,[1000 500 500 250], 1)
-
-
