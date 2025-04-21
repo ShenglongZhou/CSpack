f42d1b0295212a217628d1ae6dc511418bf2a84b
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2025-04-21 14:44:39 +0800

    Update demonIMAGE.m

--- CSpack/demonIMAGE.m
+++ CSpack/demonIMAGE.m
@@ -5,20 +5,21 @@ Img       = phantom(64*4); % 64*4 or 64*8
 sigma     = 0.01;          % 0.05 or 0.01       
 nsam      = 60;            % 40 or 60 
 
-% generate the testing data (data.A, data.At, data.b), 
-% all of them are function handle 
-[data.A,data.At,data.b,be,~,~,out0] ...
-          = getrealdata(Img,nsam,sigma,0);      
-n         = size(out0.I,2)^2;
+% generate data (A, At, b), all of them are function handle 
+[A,At,b,be,~,~,out0] = getrealdata(Img,nsam,sigma,0);      
+n                    = size(out0.I,2)^2;
 
 % set parameters for 'NL0R'
-pars.tau  = sigma;
-pars.lam  = 0.01;   
-pars.obj  = 2*norm(data.b-be)^2/3;
-solver  = { 'NL0R', 'NHTP', 'GPNP', 'IIHT'};
-for alg   = 1:length(solver)
-    out{alg} = CSsolver(data,n,solver{alg},pars); 
-    pars.s   = nnz(out{alg}.sol); 
+pars.tau    = sigma;
+pars.lambda = 0.015;   
+pars.obj    = 2*norm(b-be)^2/4;
+s           = [];
+solver      = { 'NL0R', 'PSNP', 'NHTP', 'GPNP', 'IIHT'};
+for alg     = 1:length(solver)
+    out{alg}    = CSsolver(A,At,b,n,s,solver{alg},pars); 
+    pars.lambda = 0.001;
+    pars.q      = 0.5;
+    s           = nnz(out{alg}.sol); 
 end
 
 % display results 
@@ -30,7 +31,7 @@ for alg = 1:length(solver)
     x2d   = reshape(out{alg}.sol,size(out0.I));
     Ibar  = out0.Ibar;
     x2d   = out0.W(x2d) +  Ibar;
-    subplot(2,3,alg+1+(alg>2)), imagesc(x2d), colormap(gray) 
+    subplot(2,3,alg+1), imagesc(x2d), colormap(gray) 
     psnrx(5) = psnr(out0.I,x2d, max(out0.I(:))- min(out0.I(:)));  
     ax = gca; axis(ax,'off');
     title(ax,[solver{alg},': PSNR = ',num2str(psnrx(5),'%2.2f')]);  
